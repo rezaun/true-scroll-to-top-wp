d4f681d5ee2075f1ad65c4d260445da8b0e4bc4c
Rezaun Kabir <rezaun.kabir@gmail.com>
2023-08-27 18:40:16 +0600

    git plugin name version has been done

---
+++ true-scroll-to-top-wp.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:         True Scroll To Top WP
+ * Plugin URI:          https://wordpress.org/plugins/true-scroll-to-top-wp/
+ * Description:         True Simple Scroll to top plugin will help you to enable Back to top to your Wordpress Website.
+ * Version:             1.0.0
+ * Requires at least:   5.2
+ * Requires PHP:        7.2
+ * Author:              Rezaun Kabir
+ * Author URI:          https://rezaun.netlify.app/
+ * License:             GPLv2 or later
+ * License URI:         http://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:          https://github.com/rezaun/
+ * Text-Domain:         tstt
+ */
+?>
